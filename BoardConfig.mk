67152d683629d161242f17a11a293e9e7ff877ec
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-01-08 11:19:29 +0100

    matisselte: avoid using libinit to set the default build fingerprint and radio properties

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,12 +19,11 @@ include device/samsung/matisse-common/BoardConfigCommon.mk
 # Assert
 TARGET_OTA_ASSERT_DEVICE := matisse,matisselte
 
+# Build Fingerprint
+BUILD_FINGERPRINT := samsung/matisseltexx/matisselte:5.0.2/LRX22G/T535XXU1BOL1:user/release-keys
+
 # Kernel
 TARGET_KERNEL_CONFIG := lineage_matisselte_defconfig
-
-# Init
-TARGET_INIT_VENDOR_LIB := libinit_msm8226
-TARGET_LIBINIT_MSM8226_DEFINES_FILE := device/samsung/matisselte/init/init_matisse.cpp
 
 # Radio/RIL
 include $(COMMON_PATH)/radio/single/board.mk
