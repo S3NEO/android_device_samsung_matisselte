b4608f576da002b0f3ac84640fb578fbd09b9069
matteo0026 <marmotta200026@gmail.com>
2021-03-22 21:41:42 +0100

    matisselte: use default languages, common_full_phone
    common_full_phone = common_full_tablet_wifionly + telephony.mk, so it's the same thing

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_matisselte.mk
+    device/samsung/matisselte/lineage_matisselte.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_matisselte-user \
